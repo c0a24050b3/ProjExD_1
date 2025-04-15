8ac9a4f61e69f4def8df5f0f356ed96a349cefff
c0a24050b3 <c0a24050b3@edu.teu.ac.jp>
2025-04-15 17:53:29 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -23,20 +23,28 @@ def main():
 
 
     tmr = 0
+    
+
     while True:
+        hensutate=0
+        hensuyoko=0
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst=pg.key.get_pressed() 
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            hensutate-=1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            hensutate+=1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            hensuyoko-=1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
+            hensuyoko+=2
         else:
-            kk_rct.move_ip((-1,0))
+            hensuyoko-=1
+
+        
+
+        kk_rct.move_ip((hensuyoko,hensutate))
 
         x=tmr%3200
         screen.blit(bg_img, [-x,0])
@@ -44,7 +52,7 @@ def main():
         screen.blit(bg_img2, [-x+1600,0])
         screen.blit(kk_img, kk_rct)
         pg.display.update()
-        tmr += 1        
+        tmr += 1     
         clock.tick(200)
 
 
